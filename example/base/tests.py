2f54c59f1f0a7c190336e56c534e65a80206cee4
Parkayun <iamparkayun@gmail.com>
2014-08-21 18:43:08 +0900

    test

--- example/base/tests.py
+++ example/base/tests.py
@@ -6,8 +6,10 @@ from dmu import auto_create_objects
 class DmuTests(TestCase):
     def test_auto_create_objects(self):
         to_make_model_count = 10
-        
+    
         from base.models import SampleModel
+        self.assertEqual(len(SampleModel.objects.all()), 0)
+
         for x in xrange(to_make_model_count):
             auto_create_objects(SampleModel)
 
