a5af011460d39f472b9358e843ee702937296b7f
Parkayun <iamparkayun@gmail.com>
2014-08-21 18:46:01 +0900

    modify test variable

--- example/base/tests.py
+++ example/base/tests.py
@@ -5,12 +5,12 @@ from dmu import auto_create_objects
 
 class DmuTests(TestCase):
     def test_auto_create_objects(self):
-        to_make_model_count = 10
-    
-        from base.models import SampleModel
-        self.assertEqual(len(SampleModel.objects.all()), 0)
+        object_count = 10
 
-        for x in xrange(to_make_model_count):
+        from base.models import SampleModel
+        self.assertNotEqual(len(SampleModel.objects.all()), object_count)
+
+        for x in xrange(object_count):
             auto_create_objects(SampleModel)
 
-        self.assertEqual(len(SampleModel.objects.all()), 10)
+        self.assertEqual(len(SampleModel.objects.all()), object_count)
